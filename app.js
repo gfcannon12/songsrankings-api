b3548c48c29bcf0e2cf0680fc455049a9ccfe91f
GRAY F. CANNON <gfcannon@us.ibm.com>
2017-06-12 09:56:10 -0400

    changed cron to 10am

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ let moment = require('moment');
 let CronJob = require('cron').CronJob;
 let cors = require('cors');
 
-new CronJob('45 0 * * *', function() {
+new CronJob('0 10 * * *', function() {
   dailyRankingsRequest();
 }, null, true, 'America/New_York');
 
