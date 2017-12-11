c5af47c96d64775aa016d4be19c52d1ce855da52
GRAY F. CANNON <gfcannon@us.ibm.com>
2017-12-11 08:03:37 -0500

    changed time to 8:10

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ let moment = require('moment');
 let CronJob = require('cron').CronJob;
 let cors = require('cors');
 
-new CronJob('5 8 * * *', function() {
+new CronJob('10 8 * * *', function() {
   dailyRankingsRequest();
 }, null, true, 'America/New_York');
 
